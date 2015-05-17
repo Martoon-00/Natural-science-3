4832b0cc15d2c83b899db369ccfb73950e8b324e
Koshik <elena@localhost.localdomain>
2015-05-17 21:10:21 +0300

    implement intefrace :chicken:

--- src/ru/ifmo/solvers/rungekutta/RungeKutta.java
+++ src/ru/ifmo/solvers/rungekutta/RungeKutta.java
@@ -1,49 +1,19 @@
 package ru.ifmo.solvers.rungekutta;
 
-//import com.company.Tuple;
+import ru.ifmo.lang.EqualityParameters;
+import ru.ifmo.lang.Solver;
 import ru.ifmo.lang.Tuple;
 
-public class RungeKutta {
+import java.util.ArrayList;
+
+public class RungeKutta implements Solver {
 
     private double sigma;
     private double r;
     private double b;
 
-    public RungeKutta(double sigma, double r, double b) {
-        this.sigma = sigma;
-        this.r = r;
-        this.b = b;
-    }
-
-    public Tuple solve(double x0, double y0, double z0, double delta,int steps) {
-
-        for (int i = 0; i < steps; i++ ) {
-            double kx0 = f(x0, y0);
-            double ky0 = g(x0, y0, z0);
-            double kz0 = h(x0, y0, z0);
-
-            double kx1 = f(x0 + kx0 * delta/2, y0 + ky0 * delta / 2);
-            double ky1 = g(x0 + delta / 2, y0 + ky0 * delta / 2, z0 + kz0 * delta / 2);
-            double kz1 = h(x0 + delta / 2, y0 + ky0 * delta / 2, z0 + kz0 * delta / 2);
-
-            double kx2 = f(x0 + kx1 * delta/2, y0 + ky1 * delta / 2);
-            double ky2 = g(x0 + kx1 * delta / 2, y0 + ky1 * delta / 2, z0 + kz1 * delta / 2);
-            double kz2 = h(x0 + kx1 * delta / 2, y0 + ky1 * delta / 2, z0 + kz1 * delta / 2);
-
-            double kx3 = f(x0 + kx2 * delta, y0 + ky2 * delta);
-            double ky3 = g(x0 + kx2 * delta, y0 + ky2 * delta, z0 + kz2 * delta);
-            double kz3 = h(x0 + kx2 * delta, y0 + ky2 * delta, z0 + kz2 * delta);
-
-            x0 = kx3;
-            y0 = ky3;
-            z0 = kz3;
-        }
-
-        return new Tuple(x0, y0, z0);
-    }
-
     private double f(double x, double y) {
-        return - sigma * x + sigma * y;
+        return -sigma * x + sigma * y;
     }
 
     private double g(double x, double y, double z) {
@@ -52,5 +22,42 @@ public class RungeKutta {
 
     private double h(double x, double y, double z) {
         return x * y - b * z;
+    }
+
+    @Override
+    public ArrayList<Tuple> solve(Tuple init, int n, double step, EqualityParameters params) {
+        ArrayList<Tuple> list = new ArrayList<>();
+        sigma = params.sigma;
+        b = params.b;
+        r = params.r;
+
+        double x0 = init.x;
+        double y0 = init.y;
+        double z0 = init.z;
+
+        for (int i = 0; i < n; i++) {
+            double kx0 = f(x0, y0);
+            double ky0 = g(x0, y0, z0);
+            double kz0 = h(x0, y0, z0);
+
+            double kx1 = f(x0 + kx0 * step / 2, y0 + ky0 * step / 2);
+            double ky1 = g(x0 + step / 2, y0 + ky0 * step / 2, z0 + kz0 * step / 2);
+            double kz1 = h(x0 + step / 2, y0 + ky0 * step / 2, z0 + kz0 * step / 2);
+
+            double kx2 = f(x0 + kx1 * step / 2, y0 + ky1 * step / 2);
+            double ky2 = g(x0 + kx1 * step / 2, y0 + ky1 * step / 2, z0 + kz1 * step / 2);
+            double kz2 = h(x0 + kx1 * step / 2, y0 + ky1 * step / 2, z0 + kz1 * step / 2);
+
+            double kx3 = f(x0 + kx2 * step, y0 + ky2 * step);
+            double ky3 = g(x0 + kx2 * step, y0 + ky2 * step, z0 + kz2 * step);
+            double kz3 = h(x0 + kx2 * step, y0 + ky2 * step, z0 + kz2 * step);
+
+            x0 = kx3;
+            y0 = ky3;
+            z0 = kz3;
+            list.add(new Tuple(x0, y0, z0));
+        }
+
+        return list;
     }
 }
